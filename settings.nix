5c4dd94f0e1b4c55c3effd96e5138b866adb028a
Aled Lorans <143277280+alorans@users.noreply.github.com>
2026-01-08 20:02:34 -0700

    Redo for better VM support

--- settings.nix
+++ settings.nix
@@ -1,10 +1,13 @@
 # Customize these values for your setup
 {
-  hostname = "nixosvm";
-  username = "youruser";
+  hostName = "nixosvm";
+  userName = "alorans";
+  configName = "vm";
+
+  # Localization
   timezone = "America/Phoenix";
   locale = "en_US.UTF-8";
-  
+
   # Console settings
   consoleFont = "Lat2-Terminus16";
   consoleKeyMap = "us";
